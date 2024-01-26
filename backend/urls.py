a161b7a8dad95afd8577fc4984c91047b2b65221
vingeophysicist <uobodomavin@gmail.com>
2024-01-26 02:00:06 +0100

    again

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from . import views
+from .views import retrieve_json_data
 
 
 app_name = 'backend'
 
 
 urlpatterns = [
-    path('', views.index, name ='indexpage')
-    
+    path('', views.index, name ='indexpage'),
+    path('api/<str:user_type>/', retrieve_json_data, name='retrieve_json_data')
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import requests
+
+
 
 # Create your views here.
-
+def retrieve_json_data(request, user_type):
+    
+    student = 'https://my-json-server.typicode.com/depth0/survey1/surveys'
+    bachelor = 'https://my-json-server.typicode.com/depth0/survey1/questions'
+    
+    json_link = student if user_type == 'student' else bachelor
+    
+    try:
+        response = requests.get(json_link)
+        data = response.json()
+        return JsonResponse(data, safe=False)
+    except requests.exceptions.RequestException as e:
+        return JsonResponse({'error': str(e)})
+    
+    
+    
 
 def index(request):
     return render(request, 'index.html')
+
+
